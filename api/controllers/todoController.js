57511bb04bdb9b05ffb115f23be85e87edbf0da7
LucaFeger <luca.feger@outlook.de>
2018-07-05 14:05:35 +0200

    fixed delete by inserting ne connect

--- api/controllers/todoController.js
+++ api/controllers/todoController.js
@@ -63,8 +63,9 @@ exports.getInformation = (req, res) => {
 }
 
 exports.createConnectEntry = (req, res) => {
-    connectModel.deleteMany({deviceID: req.params.deviceID}, function (err) {
+    connectModel.remove({deviceID: req.params.deviceID}, function (err) {
         if (err) res.send(err);
+        console.log("removed");
         // deleted at most one tank document
     });
 
