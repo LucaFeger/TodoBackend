4d7edef405f0bad9924112f7e1bc0a8e7d2c4fff
LucaFeger <luca.feger@outlook.de>
2018-07-05 09:04:46 +0200

    tried cors bugfix

--- index.js
+++ index.js
@@ -14,8 +14,8 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
 app.use((req, res, next) => {
-    console.log(req.url);
-    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Origin", "https://lucafeger.de");
+    res.header('Access-Control-Allow-Methods', 'GET, POST');
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
